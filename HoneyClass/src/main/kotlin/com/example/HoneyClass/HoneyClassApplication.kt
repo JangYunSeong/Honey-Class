27389a58a977db981757becbc4bff596a754f344
장윤성 <46878756+JangYunSeong@users.noreply.github.com>
2022-11-27 17:13:16 +0900

    ✨ feat: initialization

---
+++ HoneyClass/src/main/kotlin/com/example/HoneyClass/HoneyClassApplication.kt
@@ -0,0 +1,11 @@
+package com.example.HoneyClass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoneyClassApplication
+
+fun main(args: Array<String>) {
+	runApplication<HoneyClassApplication>(*args)
+}
