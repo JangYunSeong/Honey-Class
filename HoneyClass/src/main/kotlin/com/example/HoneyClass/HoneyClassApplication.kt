136bf59aea63d9db02b2cc9ed752199287885a99
yunsung <46878756+JangYunSeong@users.noreply.github.com>
2023-01-08 13:51:34 +0900

    ✨ feat: Append professor and class type

--- HoneyClass/src/main/kotlin/com/example/HoneyClass/HoneyClassApplication.kt
+++ HoneyClass/src/main/kotlin/com/example/HoneyClass/HoneyClassApplication.kt
@@ -2,8 +2,10 @@ package com.example.HoneyClass
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class HoneyClassApplication
 
 fun main(args: Array<String>) {
--- HoneyClass/src/main/kotlin/com/example/HoneyClass/repository/LectureInfoRepository.kt
+++ HoneyClass/src/main/kotlin/com/example/HoneyClass/repository/LectureInfoRepository.kt
@@ -9,6 +9,15 @@ interface LectureInfoRepository : JpaRepository<Lecture, String> {
 
     fun findByMajor(major: String) : ArrayList<Lecture>
 
-    @Query("select l from Lecture l where l.major = :major order by (l.sugangpack / l.total) DESC , (l.sugang / l.total) DESC")
+    @Query("select l from Lecture l where l.major = :major order by (l.sugangpack / l.sugang) DESC , (l.sugang / l.total) DESC")
     fun findByOrderedMajor(major: String) : ArrayList<Lecture>
+
+    @Query("select l from Lecture l where l.information like %:category% order by (l.sugangpack / l.sugang) DESC , (l.sugang / l.total) DESC")
+    fun findByCategory(category:String) : ArrayList<Lecture>
+
+    @Query("select l from Lecture l where l.professor like %:professor% order by (l.sugangpack / l.sugang) DESC")
+    fun findByProfessor(professor:String) : ArrayList<Lecture>
+
+    @Query("select l from Lecture l where l.type like %:type% order by (l.sugangpack / l.sugang) DESC")
+    fun findByType(type:String) : ArrayList<Lecture>
 }
