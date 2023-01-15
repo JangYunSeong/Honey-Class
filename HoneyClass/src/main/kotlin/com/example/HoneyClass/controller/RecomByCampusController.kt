38b5625c112c025d3e7a6a7e11a032a1655a4e9b
장윤성 <46878756+JangYunSeong@users.noreply.github.com>
2023-01-15 16:10:02 +0900

    ✨feat: append campus controller

---
+++ HoneyClass/src/main/kotlin/com/example/HoneyClass/controller/RecomByCampusController.kt
@@ -0,0 +1,19 @@
+package com.example.HoneyClass.controller
+
+import com.example.HoneyClass.dto.LectureDto
+import com.example.HoneyClass.service.RecomByCampusService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RecomByCampusController (
+    @Autowired val recomByCampusService: RecomByCampusService
+){
+
+    @GetMapping("/campus")
+    fun getRecomByCampus(): ArrayList<LectureDto> {
+        return recomByCampusService.recommendByCampus("상주캠퍼스")
+    }
+}
